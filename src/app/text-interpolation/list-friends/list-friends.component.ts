9aa550df6751667d21800fccba18ea2ff44b1891
SophieHervieu <sophie-hervieu@live.fr>
2025-02-11 09:16:20 +0100

    ajout property binding

--- src/app/text-interpolation/list-friends/list-friends.component.ts
+++ src/app/text-interpolation/list-friends/list-friends.component.ts
@@ -9,11 +9,21 @@ import { OneFriendComponent } from './one-friend/one-friend.component';
 })
 export class ListFriendsComponent {
   isDisabled: boolean = false;
+  inputValue: string = '';
+  defaultValue: string = '🥶 Aucun ami';
 
   constructor() {
     setTimeout(() => {
       this.isDisabled = true;
       alert("Trop tard ! Héhéhé");
     }, 3000);
+  }
+
+  capterEvent(event:any) {
+    this.inputValue = event.target.value;
+  }
+
+  changeText() {
+    this.defaultValue = 'Ami ajouté ! 😎';
   }
 }
